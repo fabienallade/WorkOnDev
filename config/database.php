3e1bc5c19464349afe3c95ec1c0d9e913684b144
Allade fabien <alladefabien@gmail.com>
2021-12-10 21:46:24 +0100

    work on securisation of the password

--- config/database.php
+++ config/database.php
@@ -48,8 +48,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', ''),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'workondev'),
+            'port' => env('DB_PORT', ''),
+            'database' => env('DB_DATABASE', ''),
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
